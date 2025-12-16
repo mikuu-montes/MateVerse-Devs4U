420863093ae0d6b66b39b02e70b25e69ab130256
Mica Montes <bmontes@fi.uba.ar>
2025-12-16 10:07:51 -0300

    Modifico el archivo docker-compose.yml

--- backend/data/schemas.sql
+++ backend/data/schemas.sql
@@ -1,6 +1,15 @@
 /*Estructura de como estan creados las tablas de las entidades*/
 
-CREATE TABLE usuarios(id_usuario INT PRIMARY KEY, nombre_completo VARCHAR(100) NOT NULL, nombre_usuario VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, contrasenia VARCHAR(100) NOT NULL, fecha_creacion DATE DEFAULT CURRENT_DATE, foto_perfil TEXT);
+create table usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nombre_completo VARCHAR(100) NOT NULL,
+    nombre_usuario VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    contrasenia VARCHAR(100) NOT NULL,
+    fecha_registro DATE NOT NULL DEFAULT CURRENT_DATE,
+    foto_perfil TEXT
+);
+
 /*CURRENT_DATE automaticamente la bdd lo agrega*/
 
 CREATE TABLE meme(id_meme INT PRIMARY KEY, titulo VARCHAR(100), imagen_url TEXT, descripcion TEXT, protagonistas VARCHAR(200), id_usuario INT FOREIGN KEY, id_categoria INT FOREIGN KEY, id_contexto INT FOREIGN KEY);
