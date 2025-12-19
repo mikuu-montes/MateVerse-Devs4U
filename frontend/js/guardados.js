cc3c1888eb97319afea6fecb1ff8dcee39392db5
Abril <abril.borsnich@gmail.com>
2025-12-18 20:47:51 -0400

    integracion de la pagina memes guardados

--- frontend/js/guardados.js
+++ frontend/js/guardados.js
@@ -1,10 +1,37 @@
 
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const contenedor = document.querySelector('.contenedorMemesGuardados');
+  document.addEventListener('DOMContentLoaded', async () => {
+    const contenedor = document.querySelector('.contenedorMemesGuardados');
+    
+    const idUsuarioLogueado = obtenerIdUsuarioLogueado()
 
-   const url = `api/va/usuarios/${usuarioId}/memes-guardados`;
+    const url = `/api/v1/usuarios/${idUsuarioLogueado}/memes-guardados`;
 
-   const memes_guardados = await fetch(url);
+  try {  
+    
+    const respuesta = await fetch(url);
 
-   
+     const memesGuardados = await respuesta.json();
+
+  
+
+
+    memesGuardados.forEach (meme => {
+
+      const tarjeta = document.createElement('div');
+      tarjeta.classList.add('memeTarjeta')
+      tarjeta.innerHTML = `
+        
+        <img src = "${meme.imagen_url}" >
+        <a href = "../Visualizacion Meme/index.html?id=${meme.id_meme}" class="nombreMeme">${meme.titulo}</a>
+
+        `;
+
+      contenedor.appendChild(tarjeta)
+      
+    });
+  }catch (err){
+    console.error(err);
+    alert("Error al cargar memes guardados");
+  }
+});
