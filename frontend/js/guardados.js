6701195c8fec188d0450f7dd3a1aba61070de79b
Mica Montes <bmontes@fi.uba.ar>
2026-02-05 00:48:33 -0300

    Mejoro como se ven los memes en el santuario, cambiandoles el color

--- frontend/js/guardados.js
+++ frontend/js/guardados.js
@@ -1,42 +1,40 @@
 
-  document.addEventListener('DOMContentLoaded', async () => {
-    const contenedor = document.querySelector('.contenedorMemesGuardados');
-    
-    const idUsuarioLogueado = obtenerIdUsuarioLogueado()
+document.addEventListener('DOMContentLoaded', async () => {
+  const contenedor = document.querySelector('.contenedorMemesGuardados');
+  
+  const idUsuarioLogueado = obtenerIdUsuarioLogueado()
 
-    //Si no esta logueado lo muevo a otro template.
-    if (!idUsuarioLogueado) {
-      window.location.href = "../Login Usuario/index.html";
-    }
+  //Si no esta logueado lo muevo a otro template.
+  if (!idUsuarioLogueado) {
+    window.location.href = "../Login Usuario/index.html";
+  }
 
-
-    const url = `http://localhost:3000/api/v1/usuarios/${idUsuarioLogueado}/memes-guardados`;
+  const url = `http://localhost:3000/api/v1/usuarios/${idUsuarioLogueado}/memes-guardados`;
 
   try {  
-    
     const respuesta = await fetch(url);
+    const memesGuardados = await respuesta.json();
 
-     const memesGuardados = await respuesta.json();
+    memesGuardados.forEach(meme => {
 
-  
-    memesGuardados.forEach (meme => {
-
-      const tarjeta = document.createElement('div');
-      tarjeta.classList.add('memeTarjeta')
+      const tarjeta = document.createElement('a');
+      tarjeta.classList.add('linkContainerPost');
+      tarjeta.href = `../Visualizacion Meme/index.html?id=${meme.id_meme}`;
+    
       tarjeta.innerHTML = `
-        
-        <img src = "${meme.imagen_url}" >
-        <p>${meme.titulo}</p>
-
+        <div class="containerPostGuardados">
+          <div class="postGuardados">
+            <img src="${meme.imagen_url}" alt="Meme ${meme.titulo}">
+            <div class="tituloPostGuardados">
+              <p class="tituloPost">${meme.titulo}</p>
+            </div>
+          </div>
+        </div>
       `;
-
-      tarjeta.addEventListener('click', () => {
-        window.location.href = `../Visualizacion Meme/index.html?id=${meme.id_meme}`;
-      });
-
-      contenedor.appendChild(tarjeta)
-      
+    
+      contenedor.appendChild(tarjeta);
     });
+    
   }catch (err){
     console.error(err);
     alert("Error al cargar memes guardados");
